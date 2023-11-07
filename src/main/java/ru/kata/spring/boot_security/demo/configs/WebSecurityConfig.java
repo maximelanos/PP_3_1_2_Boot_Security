94d2c533336891ffa439cbd2a9d4c5131c0e6a49
Maksim Bardikov <maximelanosx@yandex.ru>
2023-11-07 22:10:33 +0300

    fix commit

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -28,8 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").permitAll()//hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin/**").permitAll()//hasRole("ADMIN")
                 .and()
                 .formLogin().loginPage("/login").successHandler(successUserHandler)
                 .permitAll()
